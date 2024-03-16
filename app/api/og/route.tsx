ca7a3919bf3a98457213bb1e270c6a7ac628f80e
Mohit kumar singh <tihom4537@Mohits-MacBook-Air.local>
2024-03-16 14:47:57 +0530

    new files added

---
+++ app/api/og/route.tsx
@@ -0,0 +1,89 @@
+import { ImageResponse } from "@vercel/og";
+// import  safeLoad  from 'js-yaml';
+
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const base64Param = searchParams.get("base64");
+
+        if (!base64Param) {
+            throw new Error("Base64 parameter not found");
+        }
+
+        // Decode the base64 parameter
+        const decodedData = Buffer.from(base64Param, "base64").toString("utf-8");
+
+        // Now you can use the decoded data to generate the open-graph image
+        // For simplicity, I'm assuming the decoded data is the title for the image
+        const title = decodedData;
+        // Parse the YAML data
+        
+
+        // Access the AsyncAPI field
+        // const asyncAPIVersion = parsedData.asyncapi;
+        // const parsedData = JSON.parse(title);
+        // console.log(asyncAPIVersion);
+
+        const parsedData = JSON.parse(title);
+        // const docTitle = parsedData.info.title;
+        // const version =parsedData.asyncapi;
+        var asyncapiVersion = parsedData.asyncapi;
+        var docTitle = parsedData.info.title;
+        var numberOfServers = Object.keys(parsedData.servers).length;
+        var numberOfMessages = Object.keys(parsedData.channels).reduce(function (accumulator, current) {
+        return accumulator + Object.keys(parsedData.channels[current].messages).length;
+        }, 0);
+        var numberOfChannels = Object.keys(parsedData.channels).length;
+
+console.log("AsyncAPI Version:", asyncapiVersion);
+console.log("Title:", title);
+console.log("Number of Servers:", numberOfServers);
+console.log("Number of Messages:", numberOfMessages);
+console.log("Number of Channels:", numberOfChannels);
+
+        // console.log(APIVersion);
+        // console.log("hi");
+
+        // Fetch image data (replace this with your own logic)
+        const imageData = await fetch(
+            new URL("../../../assets/logo.jpeg", import.meta.url)
+        ).then((res) => res.arrayBuffer());
+
+        // Convert ArrayBuffer to base64 data URL
+        const base64ImageData = Buffer.from(imageData).toString("base64");
+        const dataUrl = `data:image/jpeg;base64,${base64ImageData}`;
+
+        return new ImageResponse(
+            <div tw=" display: flex flex-col w-full h-full items-center justify-center bg-gray-50">
+            <div tw="display: flex background-color: gray-50; justify-content: center; width: full;">
+            <img tw="w-50 h-50 rounded-lg" src={dataUrl} alt="AsyncAPI" />
+
+</div>
+
+            <div tw=" display: flex text-center">
+                <h2 tw="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900">{docTitle}</h2>
+            </div>
+            <div tw=" display: flex  justify-center w-full py-4">
+                {/* <span tw="text-indigo-600">"Title:" {docTitle}</span> */}
+                <span>"AsyncAPI Version:" {asyncapiVersion}|</span>
+                <span>"Number of Servers:" {numberOfServers}|</span>
+                <span>"Number of Messages:" {numberOfMessages}|</span>
+                <span>"Number of Channels:" {numberOfChannels}</span>
+            </div>
+           
+        </div>,
+        
+        
+        
+
+            // {
+            //     width: 2000,
+            //     height: 1000,
+            // },
+        );
+    } catch (e: any) {
+        return new Response("Failed to generate OG image", { status: 500 });
+    }
+}
